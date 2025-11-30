c549754c79975ef26d566379815728a046eae6f5
Rêd Guy <murilofch@gmail.com>
2025-11-30 08:04:30 -0300

    feat: Implementa ajustes gerais segundo feedback do designer

--- js/script.js
+++ js/script.js
@@ -153,70 +153,29 @@ document.addEventListener('DOMContentLoaded', () => {
 	});
 	// Services END
 
-	// Gallery START
-	$('.gallery-wrap a').magnificPopup({
-		type: 'image',
-		gallery: {
-			enabled: true
-		},
-		callbacks: {
-			beforeOpen: function() {
-				this.st.image.markup = this.st.image.markup.replace('mfp-figure', 'mfp-figure mfp-with-anim');
-				this.st.mainClass = this.st.el.attr('data-effect');
-			}
-		},
-	});
-
-	$('.gallery-btn a').on('click', function(e) {
-		e.preventDefault();
-		var galleryItem = $('.gallery-item');
-
-		if($(this).hasClass('is-active')) {
-			$(this).removeClass('is-active');
-			$(this).text('Ver mais');
-			galleryItem.each(function() {
-				if($(this).hasClass('is-active')) {
-					$(this).removeClass('is-active');
-					$(this).slideUp();
-				}
-			});
-		}
-		else {
-			$(this).addClass('is-active');
-			$(this).text('Ver menos');
-			galleryItem.each(function() {
-				if(!$(this).is(':visible')) {
-					$(this).addClass('is-active');
-					$(this).slideDown();
-				}
-			});
-		}
-	});
+	// Portfolio START
+    const portfolioSwiper = new Swiper('.portfolio-swiper', {
+        speed: 1000,
+        spaceBetween: 20,
+        pagination: {
+            el: '.portfolio-swiper .swiper-pagination',
+            type: 'bullets',
+            clickable: true,
+        },
+        breakpoints: {
+            320: {
+                slidesPerView: 1,
+            },
+            575: {
+                slidesPerView: 2,
+            },
+            992: {
+                slidesPerView: 3,
+            },
+        }
+    });
 	// Gallery END
 
-
-	// Reviews START
-	const reviewsSwiper = new Swiper('.reviews-swiper', {
-		speed: 1000,
-		spaceBetween: 20,
-		pagination: {
-			el: '.reviews-swiper .swiper-pagination',
-			type: 'bullets',
-			clickable: true,
-		},
-		breakpoints: {
-			320: {
-				slidesPerView: 1,
-			},
-			575: {
-				slidesPerView: 2,
-			},
-			992: {
-				slidesPerView: 3,
-			},
-		  }
-	});
-	// Reviews END
 
 	// WhatsApp Form Integration START
 	const contactForm = document.getElementById('contact-form');
